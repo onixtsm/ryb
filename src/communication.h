f5a0addd2baacb044aacb5500a1d111da8529a06
onixtsm <alexander@sautkin.com>
2023-10-23 00:05:23 +0200

    Communication module comments

--- src/communication.h
+++ src/communication.h
@@ -17,14 +17,33 @@ typedef struct {
 
 typedef uint8_t _controller_t;
 
+/* Recieves pin number [0..5] and data (uint8_t).
+ * returns error on wrong pin or unset controller
+ */
 int transmit_data(const uint8_t pin, const uint8_t data);
 
+/* Recieves pin [0..5], runs loop while the data has not been captured.
+ * TODO: make errors understandable
+ */
 uint8_t recieve_data(const uint8_t pin);
 
+/* Recieves pin number [0..5] and type [0..1]
+ * 0 - transmitting
+ * 1 - receiving
+ * returns errors on wrong pin or if all UART controllers are used
+ */
 int set_pin(const uint8_t pin, const bool type);
 
+/* Recieves pin number [0..5].
+ * sets pin as unused.
+ *returns error if pin is wrong
+ */
 int free_pin(const uint8_t pin);
 
+/* Recieves nothing.
+ *(re)sets all pins as unused.
+ *must be called before using any pin related function
+ */
 int reset_pins(void);
 
-#endif //COMMUNICATION_H
+#endif  // COMMUNICATION_H
